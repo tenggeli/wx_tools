9856deccdb911b38fe632effd0f6cd38b6ed3dbc
root <root@ycl.lvluole.cc>
2018-03-05 07:59:00 +0800

    更新

--- app/bin/manage.py
+++ app/bin/manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
-import sys
 
-reload(sys)
-sys.setdefaultencoding('utf8')
+from flask_script import Manager
+from main import app
 
-from test import app
+manager = Manager(app)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    manager.run()
--- app/bin/test.py
+++ app/bin/test.py
@@ -1,3 +1,5 @@
+# -*- coding:utf8 -*-
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +11,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
